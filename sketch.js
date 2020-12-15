c9443277b542b630d9134b8b69f4a35107cd4fb3
neha-123456 <71399848+neha-123456@users.noreply.github.com>
2020-12-15 14:44:56 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var ground, ball;
+var object, ball;
 
 function setup(){
     var canvas = createCanvas(400,400);
@@ -14,8 +14,8 @@ function setup(){
         isStatic: true
     }
 
-    ground = Bodies.rectangle(200,390,200,20,ground_options);
-    World.add(world,ground);
+    object = Bodies.rectangle(200,390,200,20,ground_options);
+    World.add(world,object);
 
     var ball_options ={
         restitution: 1.0
@@ -31,7 +31,7 @@ function draw(){
     background(0);
     Engine.update(engine);
     rectMode(CENTER);
-    rect(ground.position.x,ground.position.y,400,20);
+    rect(object.position.x,object.position.y,400,20);
 
     ellipseMode(RADIUS);
     ellipse(ball.position.x, ball.position.y, 20, 20);
